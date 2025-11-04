e5b6aec38007b6ba17bbb7df6c43e587143c6c8c
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2025-11-04 11:10:36 +0100

    Routing, child components i walidacja

---
+++ ComarchCwiczenia/ComarchCwiczenia/ComarchCwiczenia.Client/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace ComarchCwiczenia.Client.Model;
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public string Email { get; set; }
+
+    public string ConfirmEmail { get; set; }
+
+    public string Password { get; set; }
+
+    public string Address { get; set; }
+}
---
+++ ComarchCwiczenia/ComarchCwiczenia/ComarchCwiczenia.Client/Pages/BlazorValidations.razor.cs
@@ -0,0 +1,17 @@
+﻿using ComarchCwiczenia.Client.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace ComarchCwiczenia.Client.Pages;
+
+public partial class BlazorValidations
+{
+    [Inject]
+    public NavigationManager Navigation { get; set; }
+
+    public Person PersonModel { get; set; } = new();
+
+    private async Task AddPerson()
+    {
+
+    }
+}
