68332bfa22a99f044b3ab8810ab97e8f6b3d86e7
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2025-11-04 12:08:56 +0100

    Walidacja danych

---
+++ ComarchCwiczenia/ComarchCwiczenia/ComarchCwiczenia.Client/Common/PasswordValidation.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComarchCwiczenia.Client.Common;
+
+public class PasswordValidation : ValidationAttribute
+{
+    private readonly int _minLength;
+    private readonly int _maxLength;
+    private readonly bool _requireDigit;
+    private readonly bool _requireLowerCase;
+    private readonly bool _requireUpperCase;
+    private readonly bool _requireNonAlphanumeric;
+
+    public PasswordValidation(int minLength, int maxLength, bool requireDigit,
+        bool requireLowerCase, bool requireUpperCase, bool requireNonAlphanumeric)
+    {
+        _minLength = minLength;
+        _maxLength = maxLength;
+        _requireDigit = requireDigit;
+        _requireLowerCase = requireLowerCase;
+        _requireUpperCase = requireUpperCase;
+        _requireNonAlphanumeric = requireNonAlphanumeric;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        string? password = value as string;
+
+        if (password.Length < _minLength || (_maxLength > 0 && password.Length > _maxLength))
+            return new ValidationResult($"The password must be between {_minLength} and {_maxLength} characters long.");
+
+        if (_requireDigit && !password.Any(char.IsAsciiDigit))
+        {
+            return new ValidationResult("Password must contain at least one digit.");
+        }
+
+        if (_requireLowerCase && !password.Any(char.IsLower))
+        {
+            return new ValidationResult("Password must contain at least one lower letter.");
+        }
+
+        if (_requireUpperCase && !password.Any(char.IsUpper))
+        {
+            return new ValidationResult("Password must contain at least one upper letter.");
+        }
+
+        if (_requireNonAlphanumeric && password.All(char.IsLetterOrDigit))
+        {
+            return new ValidationResult("Password must contain at least one non-alphanumeric character.");
+        }
+
+        return ValidationResult.Success;
+    }
+}
--- ComarchCwiczenia/ComarchCwiczenia/ComarchCwiczenia.Client/Model/Person.cs
+++ ComarchCwiczenia/ComarchCwiczenia/ComarchCwiczenia.Client/Model/Person.cs
@@ -1,16 +1,28 @@
-﻿namespace ComarchCwiczenia.Client.Model;
+﻿using ComarchCwiczenia.Client.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace ComarchCwiczenia.Client.Model;
 
 public class Person
 {
+    [Required]
     public string Name { get; set; }
 
+    [Range(18, 99, ErrorMessage = "Please enter a valid age!")]
     public int Age { get; set; }
 
+    [Required(ErrorMessage = "Please enter your email!")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address!")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")]
     public string Email { get; set; }
 
+    [Compare(nameof(Email), ErrorMessage = "Confirm email must match!")]
     public string ConfirmEmail { get; set; }
 
+    [Required]
+    [PasswordValidation(3, 50, true, true, true, true)]
     public string Password { get; set; }
 
+    [Required, StringLength(15, MinimumLength = 5)]
     public string Address { get; set; }
 }
